abe4f18ef602021246019b0accc17be2e156dda2
Ja'mez Stokes <32825749+iamsostokes@users.noreply.github.com>
2018-03-04 21:24:06 -0500

    Add files via upload

---
+++ data-factory.js
@@ -0,0 +1,19 @@
+(function(){
+    angular.module('app')
+        .factory('DataFactory', function(){
+            var data;
+            return {
+                setData,
+                getData
+            };
+            function setData (newData) {
+                data = newData;
+            }
+
+            function getData () {
+                return data;
+            }
+
+
+        });
+})();
